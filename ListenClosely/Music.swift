4d4f6fceac432cb5bd4972df3053ada8b0a29f46
TiciaChance <laticia.chance@flatironschool.com>
2016-11-29 14:41:15 -0500

    fixed audio file bug

--- ListenClosely/Music.swift
+++ ListenClosely/Music.swift
@@ -13,12 +13,13 @@ import AVFoundation
 
 class Music: NSObject, AVAudioPlayerDelegate {
     
-    var sound1Player: AVAudioPlayer!
-
+    var sound1Player: AVAudioPlayer?
+    var sound2Player: AVAudioPlayer?
+    var sound3Player: AVAudioPlayer?
+    var sound4Player: AVAudioPlayer?
     
-    var sound2Player: AVAudioPlayer!
-    var sound3Player: AVAudioPlayer!
-    var sound4Player: AVAudioPlayer!
+    
+    
     
     var playlist = [Int]()
     var currentItem = 0
@@ -31,14 +32,15 @@ class Music: NSObject, AVAudioPlayerDelegate {
         let soundFIlePath = Bundle.main.path(forResource: "1", ofType: "wav")
         let soundFileURL = URL(fileURLWithPath: soundFIlePath!)
         
+        
         let soundFIlePath2 = Bundle.main.path(forResource: "2", ofType: "wav")
-        let soundFileURL2 = URL(fileURLWithPath: soundFIlePath!)
+        let soundFileURL2 = URL(fileURLWithPath: soundFIlePath2!)
         
         let soundFIlePath3 = Bundle.main.path(forResource: "3", ofType: "wav")
-        let soundFileURL3 = URL(fileURLWithPath: soundFIlePath!)
+        let soundFileURL3 = URL(fileURLWithPath: soundFIlePath3!)
         
         let soundFIlePath4 = Bundle.main.path(forResource: "4", ofType: "wav")
-        let soundFileURL4 = URL(fileURLWithPath: soundFIlePath!)
+        let soundFileURL4 = URL(fileURLWithPath: soundFIlePath4!)
         
         do {
             try sound1Player = AVAudioPlayer(contentsOf: soundFileURL)
@@ -50,15 +52,15 @@ class Music: NSObject, AVAudioPlayerDelegate {
             print(error)
         }
         
-        sound1Player.delegate = self
-        sound2Player.delegate = self
-        sound3Player.delegate = self
-        sound4Player.delegate = self
+        sound1Player?.delegate = self
+        sound2Player?.delegate = self
+        sound3Player?.delegate = self
+        sound4Player?.delegate = self
         
-        sound1Player.numberOfLoops = 0
-        sound2Player.numberOfLoops = 0
-        sound3Player.numberOfLoops = 0
-        sound4Player.numberOfLoops = 0
+        sound1Player?.numberOfLoops = 0
+        sound2Player?.numberOfLoops = 0
+        sound3Player?.numberOfLoops = 0
+        sound4Player?.numberOfLoops = 0
         
     }
 
--- ListenClosely/ViewController.swift
+++ ListenClosely/ViewController.swift
@@ -12,22 +12,79 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     @IBOutlet weak var startPlayButton: UIButton!
-    @IBOutlet var soundButtons: [UIButton]!
     @IBOutlet weak var levelLabel: UILabel!
     
+    @IBOutlet var soundButtons: [UIButton]!
     
     
-
+    var music = Music()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        music.setUpAudioFiles()
     }
+    
+  
+    @IBAction func redButtonTapped(_ sender: Any) {
+        highlight(button: 1)
+        music.sound1Player?.play()
+    }
+  
+    @IBAction func orangeButtonTapped(_ sender: Any) {
+        highlight(button: 2)
+        music.sound2Player?.play()
 
+    }
+    
+    @IBAction func blueButtonTapped(_ sender: Any) {
+        highlight(button: 3)
+        music.sound3Player?.play()
+
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        highlight(button: 4)
+        music.sound4Player?.play()
+
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if  music.currentItem <= music.playlist.count - 1 {
+            //play next item
+        } else {
+            music.readyForUser = true
+            //reset button highlights
+            //enable buttons
+        }
+    }
+    
+    func playNextIte() {
+        let selectedItem = music.playlist[music.currentItem]
+        switch selectedItem {
+        case 1:
+            music.sound1Player?.play()
+        case 2:
+            music.sound2Player?.play()
+        case 3:
+            music.sound2Player?.play()
+        case 4:
+            music.sound2Player?.play()
+        default:
+            break;
+        }
+    }
+    
+    func highlight(button withTag: Int) {
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
