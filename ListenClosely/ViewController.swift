1568af93cdac4509378584111e7d1c59198ee8ad
TiciaChance <laticia.chance@flatironschool.com>
2016-11-29 18:02:16 -0500

    deleted music file, combined code, debugged, debugged debugged

--- ListenClosely/ViewController.swift
+++ ListenClosely/ViewController.swift
@@ -9,111 +9,241 @@
 import UIKit
 import AVFoundation
 
+import UIKit
+import AVFoundation
+
+
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-    @IBOutlet weak var startPlayButton: UIButton!
+    @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet var soundButton: [UIButton]!
     @IBOutlet weak var levelLabel: UILabel!
     
-    @IBOutlet var soundButtons: [UIButton]!
     
+    var sound1Player:AVAudioPlayer!
+    var sound2Player:AVAudioPlayer!
+    var sound3Player:AVAudioPlayer!
+    var sound4Player:AVAudioPlayer!
     
-    var music = Music()
+    var playlist = [Int]()
+    var currentItem = 0
+    var numberOfTaps = 0
+    var readyForUser = false
+    
+    var level = 1
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudioFiles()
+    }
+    
+    func setupAudioFiles (){
         
-        music.setUpAudioFiles()
+        let soundFIlePath = Bundle.main.path(forResource: "1", ofType: "wav")
+        let soundFileURL = URL(fileURLWithPath: soundFIlePath!)
+        
+        
+        let soundFIlePath2 = Bundle.main.path(forResource: "2", ofType: "wav")
+        let soundFileURL2 = URL(fileURLWithPath: soundFIlePath2!)
+        
+        let soundFIlePath3 = Bundle.main.path(forResource: "3", ofType: "wav")
+        let soundFileURL3 = URL(fileURLWithPath: soundFIlePath3!)
+        
+        let soundFIlePath4 = Bundle.main.path(forResource: "4", ofType: "wav")
+        let soundFileURL4 = URL(fileURLWithPath: soundFIlePath4!)
+        
+        do {
+            try sound1Player = AVAudioPlayer(contentsOf: soundFileURL)
+            try sound2Player = AVAudioPlayer(contentsOf: soundFileURL2)
+            try sound3Player = AVAudioPlayer(contentsOf: soundFileURL3)
+            try sound4Player = AVAudioPlayer(contentsOf: soundFileURL4)
+            
+            
+        }catch {
+            print(error)
+        }
+        
+        sound1Player.delegate = self
+        sound2Player.delegate = self
+        sound3Player.delegate = self
+        sound4Player.delegate = self
+        
+        sound1Player.numberOfLoops = 0
+        sound2Player.numberOfLoops = 0
+        sound3Player.numberOfLoops = 0
+        sound4Player.numberOfLoops = 0
+        
+        
+        
     }
     
-  
-    @IBAction func redButtonTapped(_ sender: Any) {
-        highlight(button: 1)
-        music.sound1Player?.play()
-    }
-  
-    @IBAction func orangeButtonTapped(_ sender: Any) {
-        highlight(button: 2)
-        music.sound2Player?.play()
-
+    
+    @IBAction func soundButtonPressed(sender: AnyObject) {
+        
+        if readyForUser {
+            let button = sender as! UIButton
+            
+            switch button.tag {
+            case 1:
+                sound1Player.play()
+                checkIfCorrect(buttonPressed: 1)
+                break
+            case 2:
+                sound2Player.play()
+                checkIfCorrect(buttonPressed: 2)
+                break
+            case 3:
+                sound3Player.play()
+                checkIfCorrect(buttonPressed: 3)
+                break
+            case 4:
+                sound4Player.play()
+                checkIfCorrect(buttonPressed: 4)
+                break
+            default:
+                break
+            }
+            
+        }
+        
+        
+        
+        
     }
     
-    @IBAction func blueButtonTapped(_ sender: Any) {
-        highlight(button: 3)
-        music.sound3Player?.play()
-
+    
+    func checkIfCorrect (buttonPressed:Int) {
+        if buttonPressed == playlist[numberOfTaps] {
+            if numberOfTaps == playlist.count - 1 { // we have arrived at the last item of the playlist
+                
+                
+                
+                let when = DispatchTime.now() + 1
+                DispatchQueue.main.asyncAfter(deadline: when) {
+                    self.nextRound()
+                    
+                }
+                return
+            }
+            
+            numberOfTaps += 1
+        }else{ // GAME OVER
+            resetGame()
+        }
     }
     
-    @IBAction func greenButtonTapped(_ sender: Any) {
-        highlight(button: 4)
-        music.sound4Player?.play()
-
+    
+    func resetGame() {
+        level = 1
+        readyForUser = false
+        numberOfTaps = 0
+        currentItem = 0
+        playlist = []
+        levelLabel.text = "GAME OVER"
+        startGameButton.isHidden = false
+        disableButtons()
     }
     
-    @IBAction func playButtonTapped(_ sender: Any) {
+    func nextRound() {
+        
+        level += 1
+        levelLabel.text = "Level \(level)"
+        readyForUser = false
+        numberOfTaps = 0
+        currentItem = 0
+        disableButtons()
+        
         
         let randomNumber = Int(arc4random_uniform(4) + 1)
-        music.playlist.append(randomNumber)
-        startPlayButton.isHidden = true
+        playlist.append(randomNumber)
+        
         playNextItem()
+        
+        
+        
     }
     
+    @IBAction func startGame(sender: AnyObject) {
+        levelLabel.text = "Level 1"
+        disableButtons()
+        let randomNumber = Int(arc4random_uniform(4) + 1)
+        playlist.append(randomNumber)
+        startGameButton.isHidden = true
+        playNextItem()
+        
+        
+    }
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        if  music.currentItem <= music.playlist.count - 1 {
-            //play next item
-        } else {
-            music.readyForUser = true
-            //reset button highlights
-            //enable buttons
+        
+        
+        if currentItem <= playlist.count - 1 {
+            playNextItem()
+        }else{
+            readyForUser = true
+            resetButtonHighlights()
+            enableButtons()
         }
+        
+        
     }
     
-    func playNextItem() {
-        let selectedItem = music.playlist[music.currentItem]
+    func playNextItem () {
+        let selectedItem = playlist[currentItem]
+        
         switch selectedItem {
         case 1:
-            music.sound1Player?.play()
+            highlightButtonWithTag(tag: 1)
+            sound1Player.play()
+            break
         case 2:
-            music.sound2Player?.play()
+            highlightButtonWithTag(tag: 2)
+            sound2Player.play()
+            break
         case 3:
-            music.sound2Player?.play()
+            highlightButtonWithTag(tag: 3)
+            sound3Player.play()
+            break
         case 4:
-            music.sound2Player?.play()
+            highlightButtonWithTag(tag: 4)
+            sound4Player.play()
+            break
         default:
             break;
         }
         
-        music.currentItem += 1
+        currentItem += 1
+        
     }
     
-    func highlight(button withTag: Int) {
-        
-        switch withTag {
+    func highlightButtonWithTag (tag:Int) {
+        switch tag {
         case 1:
-            resetButtonHightlights()
-            soundButtons[withTag - 1].setImage(#imageLiteral(resourceName: "redPressed"), for: .normal)
+            resetButtonHighlights()
+            soundButton[tag - 1].setImage(#imageLiteral(resourceName: "redPressed"), for: .normal)
         case 2:
-            resetButtonHightlights()
-            soundButtons[withTag - 1].setImage(#imageLiteral(resourceName: "yellowPressed"), for: .normal)
+            resetButtonHighlights()
+            soundButton[tag - 1].setImage(#imageLiteral(resourceName: "yellowPressed"), for: .normal)
         case 3:
-            resetButtonHightlights()
-            soundButtons[withTag - 1].setImage(#imageLiteral(resourceName: "bluePressed"), for: .normal)
+            resetButtonHighlights()
+            soundButton[tag - 1].setImage(#imageLiteral(resourceName: "bluePressed"), for: .normal)
         case 4:
-            resetButtonHightlights()
-            soundButtons[withTag - 1].setImage(#imageLiteral(resourceName: "greenPressed"), for: .normal)
+            resetButtonHighlights()
+            soundButton[tag - 1].setImage(#imageLiteral(resourceName: "greenPressed"), for: .normal)
         default:
-            break;
-
+            break
         }
     }
     
-    func resetButtonHightlights() {
-        soundButtons[0].setImage(#imageLiteral(resourceName: "redPressed"), for: .normal)
-        soundButtons[1].setImage(#imageLiteral(resourceName: "yellowPressed"), for: .normal)
-        soundButtons[2].setImage(#imageLiteral(resourceName: "bluePressed"), for: .normal)
-        soundButtons[3].setImage(#imageLiteral(resourceName: "greenPressed"), for: .normal)
-
+    func resetButtonHighlights () {
+        soundButton[0].setImage(#imageLiteral(resourceName: "red"), for: .normal)
+        soundButton[1].setImage(#imageLiteral(resourceName: "yellow"), for: .normal)
+        soundButton[2].setImage(#imageLiteral(resourceName: "blue"), for: .normal)
+        soundButton[3].setImage(#imageLiteral(resourceName: "green"), for: .normal)
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -121,5 +251,24 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
     
     
+}
+
+//MARK: DISABLE OR ENABLE BUTTONS
+
+extension ViewController {
+    
+    
+    func disableButtons () {
+        for button in soundButton {
+            button.isUserInteractionEnabled = false
+        }
+    }
+    
+    func enableButtons () {
+        for button in soundButton {
+            button.isUserInteractionEnabled = true
+        }
+    }
+
 }
 
